ff71117b0e9061c02c52feaf7c7b8b735fa5f7ab
Diego <diego2000avelar@gmail.com>
2021-02-05 21:06:46 -0600

    form boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Form, TextField, SelectField, SubmitButton } from './form/elements';
+import * as Yup from 'yup';
+
 
 function App() {
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    role: ""
+  });
+
+  const FormSchema = Yup.object().shape({
+    name: Yup.string()
+          .required('Required')
+          .min(5, "Required"),
+    email: Yup.string().email()
+        .required('Required')
+        .min(1, "Required"),
+    role: Yup.string().oneOf(['admin', 'user'])
+          .required('Required')
+          .min(1, "Required"),
+});
+
+  const onSubmit = (values, { setSubmitting, resetForm, setStatus }) => {
+    console.log(values);
+    setFormData(values);
+    setSubmitting(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Form
+            enableReinitialize
+            initialValues={formData}
+            onSubmit={onSubmit}
+            validationSchema={FormSchema}
+          >
+            <div>
+              <TextField
+                name="name"
+                label="Name"
+              />
+            </div>
+
+            <div>
+              <TextField 
+                name="email"
+                label="Email"
+              />
+            </div>
+
+            <div>
+              <SelectField 
+                name="role"
+                label="Role"
+                options={[
+                  {
+                    label: "Admin",
+                    value: "admin"
+                  },
+                  {
+                    label: "User",
+                    value: "user"
+                  }
+                ]}
+              />
+            </div>
+
+            <SubmitButton
+              title="Submit"
+            />
+          </Form>
     </div>
   );
 }
